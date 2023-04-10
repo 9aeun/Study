98e15fe7cbb8f0867669baef2ebf58b108858775
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-04-10 21:16:40 +0900

    Update PostController.java

--- spring_boot/api/controller/PostController.java
+++ spring_boot/api/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.springboot.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 // 컨트롤러 클래스에서 공통 URL 설정
 @RestController
@@ -13,6 +14,25 @@ public class PostController {
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
+    }
+
+   // @RequestBody를 활용한 POST 메서드 구현
+    // http://localhost:8080/api/v1/post-api/member
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+    
+    // DTO 객체를 활용한 POST API 구현
+    @PostMapping(value = "/member2")
+    public String postMemberDto(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
     }
 }
 
