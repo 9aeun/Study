64b373535540aae45bd6e8a79b7856612fffe5a2
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-04-18 22:17:28 +0900

    Update GetController(logger).java

--- spring_boot/api/controller/GetController(logger).java
+++ spring_boot/api/controller/GetController(logger).java
@@ -1,3 +1,7 @@
+// GetController에 Logger 기능을 추가.
+// 이를 사용하려면 logback-Spring.xml의 15~28번 및 34번 줄을 지워 파일로 저장되는 로그는 없도록 설정을 변경한 후 진행.
+// 이 Appender를 지우려면 따로 설정한 Root나 Logger 영역에서도 레퍼런스로 잡혀 있지는 않은지 확인한 후 제거해야함.
+    
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
