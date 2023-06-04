1b1fe8cadd5e12ac183120ee574f3c9eceee6961
GaEun Kim <109353972+9aeun@users.noreply.github.com>
2023-06-04 11:35:19 +0900

    Update SwaggerConfiguration.java

--- spring_boot/chapter6/src/main/java/com/springboot/jpa/data/config/SwaggerConfiguration.java
+++ spring_boot/chapter6/src/main/java/com/springboot/jpa/data/config/SwaggerConfiguration.java
@@ -16,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfigration {
+public class SwaggerConfiguration {
 
     // http://localhost:8080/swagger-ui.html
     @Bean
